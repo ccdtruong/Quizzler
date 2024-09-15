0a188872d2e2a41ed5cf1b67a2b681016dea3ead
ccdtruong <ccdtruong@gmail.com>
2024-09-15 22:01:08 +0700

    Up project

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        UIUpdate()
+    }
+
+    @IBAction func buttonAnswerPressed(_ sender: UIButton) {
+        
+        let userAnswser = sender.currentTitle!
+        
+        let result = quizBrain.checkAnswer(userAnswser)
+        
+        if result {
+            sender.backgroundColor = UIColor.green
+        }
+        else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(UIUpdate), userInfo: nil, repeats: false)    }
+    @objc func UIUpdate(){
+        questionText.text = quizBrain.getQuestion()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+}
+
